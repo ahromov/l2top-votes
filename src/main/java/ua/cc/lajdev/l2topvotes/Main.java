7a63a6ee4f2e0d03a4caec7f40cc57e2ee184334
Андрій <Andrii Hromov>
2019-12-17 21:28:32 +0200

    init

---
+++ src/main/java/ua/cc/lajdev/l2topvotes/Main.java
@@ -0,0 +1,139 @@
+package ua.cc.lajdev.l2topvotes;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.util.Properties;
+import java.util.Random;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class Main {
+
+	private static String connectionUrl;
+	private static String dbLogin;
+	private static String dbPassword;
+	private static Properties properties = new Properties();
+
+	static {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			properties.load(new FileInputStream("votes.properties"));
+			connectionUrl = properties.getProperty("db.url");
+			dbLogin = properties.getProperty("db.login");
+			dbPassword = properties.getProperty("db.password");
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException, ParseException {
+
+		try (Connection connection = DriverManager.getConnection(connectionUrl, dbLogin, dbPassword);
+				BufferedReader in = new BufferedReader(
+						new InputStreamReader(new URL(properties.getProperty("votes.url")).openStream()));) {
+			String date;
+			String charName;
+			String inputLine = null;
+			boolean isReadedFirstLine = false;
+
+			while ((inputLine = in.readLine()) != null) {
+				if (isReadedFirstLine == false) {
+					isReadedFirstLine = true;
+					continue;
+				} else {
+					date = inputLine.substring(0, 19);
+					charName = inputLine.substring(20).trim();
+
+					if (isRecordsPresent(date, charName, connection))
+						continue;
+					else {
+						int charId = getIdByName(charName, connection);
+
+						if (charId != 0) {
+							addReward(charId, connection);
+							insertRecord(date, charName, connection);
+						} else
+							continue;
+					}
+				}
+			}
+		} catch (SQLException | IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static boolean isRecordsPresent(String date, String name, Connection connection) {
+
+		try (Statement statement = connection.createStatement();) {
+			ResultSet rs = statement
+					.executeQuery("SELECT * FROM votes WHERE date = '" + date + "' AND char_name = '" + name + "'");
+			if (rs.next()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+
+	}
+
+	private static void insertRecord(String date, String name, Connection connection) {
+
+		try (PreparedStatement statement = connection
+				.prepareStatement("INSERT INTO votes (date, char_name) VALUES (?,?)");) {
+
+			statement.setString(1, date);
+			statement.setString(2, name);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static int getIdByName(String name, Connection connection) {
+
+		try (Statement statement = connection.createStatement();) {
+			ResultSet rs = statement.executeQuery("SELECT * FROM characters WHERE char_name = '" + name + "'");
+			if (rs.next())
+				return rs.getInt(2);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return 0;
+
+	}
+
+	private static void addReward(int charId, Connection connection) {
+
+		try (PreparedStatement statement = connection
+				.prepareStatement("INSERT INTO items (owner_id, object_id, item_id, count, loc) VALUES (?,?,?,?,?)");) {
+			statement.setInt(1, charId);
+			statement.setInt(2, new Random().nextInt(900000000));
+			statement.setInt(3, 4356);
+			statement.setInt(4, new Random().nextInt(10));
+			statement.setString(5, "INVENTORY");
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/test/java/ua/cc/lajdev/l2topvotes/L2topVotesApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.cc.lajdev.l2topvotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2topVotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
